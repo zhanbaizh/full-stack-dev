f0053c320b07c365a7b67ca91780a7af42cadf2f
Zhandos Baizhuma <baizhuma.zhandos@gmail.com>
2024-11-16 08:13:07 +0500

    client-server interactions script

---
+++ dev-core-103/dev-core-103_lesson1_client_server_sim.py
@@ -0,0 +1,10 @@
+def client_request():
+    print('Client is sending query to sever')
+
+def server_response():
+    print('Server is processing query')
+    print('Servers response: Welcome to Internet')
+
+
+client_request()
+server_response()
